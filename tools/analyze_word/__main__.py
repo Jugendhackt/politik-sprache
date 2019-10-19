767cd3aa5081e663f3df6b430afb40002696493e
Moritz Krautwald <moritz.krautwald@outlook.de>
2019-10-19 17:05:23 +0200

    feat: added click for nice klieh

--- tools/analyze_word/__main__.py
+++ tools/analyze_word/__main__.py
@@ -1,9 +1,16 @@
 from ..read_data import parse_data, write_json
 from . import analyze_words
+import click, json
 
+@click.command()
+@click.argument('input_json')
+@click.argument('output_json')
+def main(input_json, output_json):
+    with open(input_json, 'r') as f:
+        input_obj = json.load(f)
+    output_obj = analyze_words(input_obj)
+    with open(output_json, 'w') as f:
+        json.dump(output_obj, f)
 
 if __name__ == "__main__":
-    statements = parse_data("data/qual-o-mat/")
-    parties = analyze_words(statements)
-    write_json("data/words.json", parties)
-
+    main()
